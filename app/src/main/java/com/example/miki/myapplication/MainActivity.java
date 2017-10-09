fc188222e401032fa0e13f201049fcd77069b220
mikikeselj <miroslav_sekulic@outlook.com>
2017-10-09 21:41:43 +0200

    nova

--- app/src/main/java/com/example/miki/myapplication/MainActivity.java
+++ app/src/main/java/com/example/miki/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.miki.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+       }
 
 
 }
