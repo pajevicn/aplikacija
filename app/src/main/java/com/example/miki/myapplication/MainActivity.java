9600af576bd10b045e45089d69c6563cf9815832
mikikeselj <miroslav_sekulic@outlook.com>
2017-10-09 23:59:08 +0200

    menu done

--- app/src/main/java/com/example/miki/myapplication/MainActivity.java
+++ app/src/main/java/com/example/miki/myapplication/MainActivity.java
@@ -1,17 +1,42 @@
 package com.example.miki.myapplication;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private DrawerLayout nDrawerLayout;
+    private ActionBarDrawerToggle nToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        nToggle = new ActionBarDrawerToggle(this, nDrawerLayout, R.string.open, R.string.close);
+
+
+        nDrawerLayout.addDrawerListener(nToggle);
+        nToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
        }
 
+       @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+           if (nToggle.onOptionsItemSelected(item)) {
+               return true;
+           }
+
+           return super.onOptionsItemSelected(item);
+       }
 
 }
