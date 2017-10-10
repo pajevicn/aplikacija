255b2885e55dca783c6b2c022c6cd039a0b452e8
mikikeselj <miroslav_sekulic@outlook.com>
2017-10-10 22:07:15 +0200

    menu img

--- app/src/main/java/com/example/miki/myapplication/MainActivity.java
+++ app/src/main/java/com/example/miki/myapplication/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.miki.myapplication;
 
+
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.MenuItem;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         nDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         nToggle = new ActionBarDrawerToggle(this, nDrawerLayout, R.string.open, R.string.close);
 
@@ -26,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
         nToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.logo3); //also displays wide logo
+        
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+
 
 
        }
